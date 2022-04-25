497597658798829a7a693870fcb6a73d85ffabdf
Jesher Minelli <jesheralves@claretiano.edu.br>
2022-04-24 22:38:24 -0300

    meu projeto básico node para praticar docker

--- 1_imagens_e_containers/my_project_jesher/app.js
+++ 1_imagens_e_containers/my_project_jesher/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Olá minha imagem!')
+  res.send('Olá mundo! essa é minha primeira imagem.')
 })
 
 app.listen(port, () => {
